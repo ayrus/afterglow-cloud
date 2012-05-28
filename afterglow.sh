b677ed02bc4c0d7de7b06fadb819fa120d6b1a7e
ayrus <surya@hakc.net>
2012-05-28 22:03:26 +0530

    Changed the paramater structure for afterglow.sh. Added check to see if the ouput file is > 0 bytes.

--- afterglow.sh
+++ afterglow.sh
@@ -6,13 +6,13 @@
 # neato [2]. 
 #
 # Usage:
-# 	afterglow.sh "path_to_csv_file" "path_to_colour_property_file" "arguments"
-#				"path_to_resulting_output_file" "path_to_afterglow.pl"
+# 	afterglow.sh "path_to_csv_file" "path_to_colour_property_file" 
+#				"path_to_resulting_output_file" "path_to_afterglow.pl" "arguments"
 #
 # Note: Paths can be absolute or relative.
 # 		For a list of 'arguments' please see [1].
 #
-# Example usage: ./afterglow.sh "data/firewall.csv" "sample.properties" "-d -e 1.5" "images/firewall.gif" "afterglow/src/afterglow.pl"
+# Example usage: ./afterglow.sh data/firewall.csv sample.properties images/firewall.gif afterglow/src/afterglow.pl -d -e 1.5
 #
 # Exit status:
 #	0: On successful image creation.
@@ -30,18 +30,22 @@
 
 #Path to a CSV data file.
 dataFile="$1"
+	shift
 
 #Path to a colour property file (if any).
-propertyFile="$2"
-
-#Additional parameters (if any) to be passed to AfterGlow.
-args="$3"
+propertyFile="$1"
+	shift
 
 #Path to create the output file, including its name and extension.
-outputFile="$4"
+outputFile="$1"
+	shift
 
 #Complete path to afterglow.pl, including its name and extension.
-afPath="$5"
+afPath="$1"
+	shift
+
+#Additional parameters (if any) to be passed to AfterGlow.
+args="$@"
 
 #Sanitize inputs -- Check if datafile is present.
 	if [ ! -e "$dataFile" ]
@@ -81,10 +85,11 @@ afPath="$5"
 	fi
 
 #Everything looks good for now; render the graph.
-perl "$afPath" -i "$dataFile" -c "$propertyFile" "$args" | neato -Tgif -o "$outputFile"
+perl "$afPath" -i "$dataFile" -c "$propertyFile" $args | neato -Tgif -o "$outputFile"
 
-#Check if the output was successfuly rendered.
-	if [ -e "$outputFile" ]
+#Check if the output was successfuly rendered and that the output file is
+#more than 0 bytes in size.
+	if [ -e "$outputFile" ] && [ -s "$outputFile" ]
 	then
 		exit 0	
 	else
