db1ca9c23796cc04220b9bf8cbd7882ac096839c
ayrus <surya@hakc.net>
2012-06-11 03:59:53 +0530

    Added color picker and some custom JS events for the forms view.

---
+++ afterglow_cloud/afterglow_cloud/app/static/js/form.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+    $('#id_textLabel').miniColors();
+
+    $("#id_overrideEdge").click(function () { 
+        toggleShowOverrideInput();
+    });
+    
+    $("#settingsLabel").click(function () { 
+        toggleShowMainSettings();
+    });
+    
+});
+
+function toggleShowOverrideInput(){
+
+     if($('#id_overrideEdge').attr('checked')){
+        $('#id_overrideEdgeLength').show();
+     }else{
+        $('#id_overrideEdgeLength').hide();
+     }
+}
+
+function toggleShowMainSettings(){
+    $('#mainSettings').slideToggle(('slow'));   
+}
+
+
